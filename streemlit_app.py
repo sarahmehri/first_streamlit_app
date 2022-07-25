99018b4a11882af419b25d353b3af04b1a091ded
Sarah Mehri <72160953+sarahmehri@users.noreply.github.com>
2022-07-25 12:50:07 -0700

    Update streemlit_app.py

--- streemlit_app.py
+++ streemlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
 streamlit.title('my parent healthy dinner')
 streamlit.header('Break Fast Menu')
-streamlit.text('Eggs and fruits')
+streamlit.text('🥚🍒🍓🍅Eggs and fruits')
+streamlit.text('🥟🧈🥨🥯Pancake with smoothie')
+streamlit.text('🥑🥟Avocado Toast')
+streamlit.header('🍓🍒🍌🍍🥭🍎🍏🍉🍈🥝🥥🍇Build your own fruit smothie')
+
+
